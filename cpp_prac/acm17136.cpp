2d4a7844bb338ffb361805c4367661e565c9c518
2013011491 <szerencse5@hanyang.ac.kr>
2020-05-21 21:13:12 +0900

    algo_prac

---
+++ cpp_prac/acm17136.cpp
@@ -0,0 +1,13 @@
+//200518 forsds gold3
+#include<iostream>
+
+using namespace std;
+
+int board[12][12]={0,};
+
+int main(void)
+{
+    for(int i=1; i<=10; ++i){for(int j=1; j<=10; ++j){cin>>board[i][j];}}
+    
+    return 0;
+}
